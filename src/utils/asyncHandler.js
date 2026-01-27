b05ce849c1bcc3d1a596be7d5804e4d44e81454b
Arslan Choudhary <charslan00713@gmil.com>
2026-01-27 23:25:25 +0500

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHanlder = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).then((err) =>
+            next(err)
+        );
+    };
+};
+
+export default asyncHanlder;
+
+// const asyncHanlder = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message,
+//         });
+//     }
+// };
