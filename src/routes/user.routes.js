e632eb593990eb7fe2ca5b9c32d00da1e431c033
Arslan Choudhary <charslan00713@gmil.com>
2026-02-12 22:29:28 +0500

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,5 +17,26 @@ userRouter.route("/login").post(UserController.loginUser);
 // secure routes
 userRouter.route("/logout").post(verifyJWT, UserController.logoutUser);
 userRouter.route("/refresh-token").post(UserController.refreshAccessToken);
+userRouter
+    .route("/change-password")
+    .post(verifyJWT, UserController.changeCurrentPassword);
+userRouter.route("/current-user").get(verifyJWT, UserController.getCurrentUser);
+userRouter
+    .route("/update-account")
+    .patch(verifyJWT, UserController.updateAccountDetails);
+userRouter
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), UserController.updateUserAvatar);
+userRouter
+    .route("/cover-image")
+    .patch(
+        verifyJWT,
+        upload.single("coverImage"),
+        UserController.updateUserCoverImage
+    );
+userRouter
+    .route("/c/:username")
+    .get(verifyJWT, UserController.getUserChannelProfile);
+userRouter.route("/history").get(verifyJWT, UserController.getWatchHistory);
 
 export default userRouter;
